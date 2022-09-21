588979d8187068e0d94c45d3fb35598ae28d2464
Federico Paolinelli <fpaoline@redhat.com>
2022-09-21 17:21:42 +0200

    Add lower prio to the sbr rule
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- node/vrf/setup_sbr.sh
+++ node/vrf/setup_sbr.sh
@@ -4,4 +4,4 @@
 # route the traffic to the veth leg in default vrf
 echo 200 tored >> /etc/iproute2/rt_tables
 ip route add default via 192.169.1.2 table tored
-ip rule add pref 999 from 192.168.1.1 lookup tored
+ip rule add pref 1001 from 192.168.1.1 lookup tored
